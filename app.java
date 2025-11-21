45d849a406928995e123dd7b0aa2751e61c1a329
Alekhya Vankayala <92820971+AlekhyaVankayala09@users.noreply.github.com>
2025-11-21 10:49:29 +0530

    Update app.java

--- app.java
+++ app.java
@@ -10,3 +10,9 @@ public class Main {
     System.out.println("Hello World");
   }
 }
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
