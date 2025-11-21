5c6d76fb80e734e5d65c9bdcfae13dc1f3fa8eed
Alekhya Vankayala <92820971+AlekhyaVankayala09@users.noreply.github.com>
2025-11-21 10:47:30 +0530

    Update app.java

--- app.java
+++ app.java
@@ -4,3 +4,9 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
